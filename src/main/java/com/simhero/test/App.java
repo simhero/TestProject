40d41fab0122c6649123bf33fc757e89d32d5d99
simhero <simhero@gmail.com>
2017-04-14 17:01:44 +0900

    init commit

---
+++ src/main/java/com/simhero/test/App.java
@@ -0,0 +1,14 @@
+package com.simhero.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("111");
+    }
+}
